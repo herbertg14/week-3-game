06299f691564b02c23483e24016ffb3b10c63fa7
herbertg14 <herbert.gutierrez@utexas.edu>
2016-05-11 18:48:27 -0500

    working on helper functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,27 @@
 
 var object1 = {
-	word: "testing",
-	guesses: 10,
-	gotIt: false,
-	wrong:[]
+	word: "hello",
+	wordArray:[],
+	guessesLeft: 10,
+	gotWord: false,
+	wrongLetters:[],
+	blankAndLetters:[],
+	failed: false
 
 };
 
 var object2 = {
 	word: "other",
-	guesses: 5,
-	gotIt: false,
-	wrong:[]
-}
+	wordArray: [],
+	guessesLeft: 10,
+	gotWord: false,
+	wrongLetters:[],
+	blankAndLetters:[],
+	failed: false
+};
 
 function pick(){
-	// number = Math.floor(1 + Math.random() * 5)
-	number = Math.floor(1 + Math.random() * 2)
+	number = Math.floor((Math.random() * 2) + 1);
 	// console.log(number)
 
 	if (number == 1){
@@ -27,44 +32,73 @@ function pick(){
 	}
 }
 
-
-function playGame(object){
-	// console.log("----------");
-	// console.log(object);
-	// console.log(object.word);
-	document.write("Press any key to get started!<br>");
-	document.write("current word has " + object.word.length + " letters");
-	dou
-
-	word = object.word
-	
-	while ((object.guesses > 0) && (object.gotIt == false)){
-
-		document.onkeyup = function(event){
-			var letter = String.fromCharCode(event.keyCode).toLowerCase();
-			
-			for (){}
-				// if (letter == 'd'){
-				// 	alert(object.word)
-				// }
-		}
-
-		// console.log(object.guesses);
-		object.guesses--; 
-
-
-
-	}
+function update(object){
+	//update word to guess
+	//upddate wrong guesses 
+	//update guesses left
+	//update win counter 
+	//update loss counter
 
 }
 
+function checkGuess(object, guess){
 
-// document.onkeyup = function(event){
-// 	var letter = String.fromCharCode(event.keyCode).toLowerCase();
+		// for each letter of the word
+		for (var i = 0; i < object.wordArray.length; i++){
 
-// 	if (letter == 'd'){
-// 		alert("worked")
-// 	}
-// }
+			// If guess was correct
+			if (guess == object.wordArray[i]){
+				object.blankAndLetters[i] == guess;
+			}
+
+			// If guess is incorrect
+			else{
+				var inArray = false;
+
+				//check to see if word is already in the list of wrong letters
+				for (var j=0; j < object.wrongLetters.length; j++){
+					if (object.wrongLetters[j] == guess){
+						inArray = true; 
+						break;
+					}
+				}
+
+				//if the word is not already in list of wrong words
+				if (inArray == false){
+					object.wrongLetters.push(guess);
+					if (object.guessesLeft == 0){
+						object.failed = true;
+					}
+					else{
+						object.guessesLeft-=1;	
+					}
+				}
+			}
+		}
+
+
+		//check to see if the word is guessed
+		var checkWord = object.blankAndLetters.join("");
+		if (checkWord == object.word){
+			object.gotWord = true;
+		}
+
+		//update the script and game
+		// update(object)
+}
+
+function playGame(object){
+	object.wordArray = object.word.split("");
+	console.log(object.wordArray);
+
+	for(var i=1; i<object.wordArray.length; i++){
+		object.blankAndLetters.push("_");
+	}
+
+	console.log(object.blankAndLetters);
+	document.getElementById("wordToGuess").innerHTML = object.blankAndLetters.join(" ");
+	document.getElementById("guessesLeft").innerHTML = object.guessesLeft;
+}
+
 
 pick()
